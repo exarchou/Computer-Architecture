ff40c746383f3c350eb8cb0024ca2871baf7f0de
Dimitrios-Marios Exarchou <58265522+exarchou@users.noreply.github.com>
2020-12-12 22:42:57 +0200

    Add files via upload

---
+++ Lab_2/question1_3.sh
@@ -0,0 +1,9 @@
+./build/ARM/gem5.opt -d spec_results/specbzip_1 configs/example/se.py --cpu-type=MinorCPU --cpu-clock=1GHz --caches --l2cache -c spec_cpu2006/401.bzip2/src/specbzip -o "spec_cpu2006/401.bzip2/data/input.program 10" -I 100000000
+
+./build/ARM/gem5.opt -d spec_results/specmcf_1 configs/example/se.py --cpu-type=MinorCPU --cpu-clock=1GHz --caches --l2cache -c spec_cpu2006/429.mcf/src/specmcf -o "spec_cpu2006/429.mcf/data/inp.in" -I 100000000
+
+./build/ARM/gem5.opt -d spec_results/spechmmer_1 configs/example/se.py --cpu-type=MinorCPU --cpu-clock=1GHz --caches --l2cache -c spec_cpu2006/456.hmmer/src/spechmmer -o "--fixed 0 --mean 325 --num 45000 --sd 200 --seed 0 spec_cpu2006/456.hmmer/data/bombesin.hmm" -I 100000000
+
+./build/ARM/gem5.opt -d spec_results/specsjeng_1 configs/example/se.py --cpu-type=MinorCPU --cpu-clock=1GHz --caches --l2cache -c spec_cpu2006/458.sjeng/src/specsjeng -o "spec_cpu2006/458.sjeng/data/test.txt" -I 100000000
+
+./build/ARM/gem5.opt -d spec_results/speclibm_1 configs/example/se.py --cpu-type=MinorCPU --cpu-clock=1GHz --caches --l2cache -c spec_cpu2006/470.lbm/src/speclibm -o "20 spec_cpu2006/470.lbm/data/lbm.in 0 1 spec_cpu2006/470.lbm/data/100_100_130_cf_a.of" -I 100000000
